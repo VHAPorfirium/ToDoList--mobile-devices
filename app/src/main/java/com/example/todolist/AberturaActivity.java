7d08c3332cefba8891cacefab46bb8c2d384a0e3
Victor Hugo <vhaporfiro@gmail.com>
2025-03-07 09:45:03 -0300

    Configurando tela de abertura e manifest

--- app/src/main/java/com/example/todolist/AberturaActivity.java
+++ app/src/main/java/com/example/todolist/AberturaActivity.java
@@ -1,6 +1,8 @@
 package com.example.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AberturaActivity extends AppCompatActivity {
+public class AberturaActivity extends AppCompatActivity implements Runnable {
+
+    Thread thread;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_abertura);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Inicializa a Thread que controla o splash
+        handler = new Handler();
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Pausa de 2 segundos
+            Thread.sleep(2000);
+
+            // Após o tempo inicia a MainActivity
+            Intent intent = new Intent(AberturaActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
