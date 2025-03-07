647b74afe49d70282c4464c34fad50405cab3735
Victor Hugo <vhaporfiro@gmail.com>
2025-03-07 09:56:00 -0300

    Configurando tela de boas vindas e sincronizando abertura de telas

--- app/src/main/java/com/example/todolist/TelaBoasVindasActivity.java
+++ app/src/main/java/com/example/todolist/TelaBoasVindasActivity.java
@@ -10,7 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AberturaActivity extends AppCompatActivity implements Runnable {
+public class TelaBoasVindasActivity extends AppCompatActivity {
 
     Thread thread;
     Handler handler;
@@ -19,28 +19,25 @@ public class AberturaActivity extends AppCompatActivity implements Runnable {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_abertura);
-
+        setContentView(R.layout.activity_tela_boas_vindas);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Inicializa a Thread que controla o splash
         handler = new Handler();
-        thread = new Thread(this);
+        thread = new Thread(this::run);
         thread.start();
     }
 
-    @Override
     public void run() {
         try {
             // Pausa de 2 segundos
             Thread.sleep(2000);
 
-            // Após o tempo inicia a Tela de boas vindas
-            Intent intent = new Intent(AberturaActivity.this, TelaBoasVindasActivity.class);
+            // Após o tempo inicia a MainActivity
+            Intent intent = new Intent(TelaBoasVindasActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
 
